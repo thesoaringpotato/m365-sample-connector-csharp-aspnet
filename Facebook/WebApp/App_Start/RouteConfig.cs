169d75124cde937fbf2cc6584992c91427c06394
thesoaringpotato <alfredo.e.moreno@gmail.com>
2022-10-13 14:10:51 -0700

    Update RouteConfig.cs

--- Facebook/WebApp/App_Start/RouteConfig.cs
+++ Facebook/WebApp/App_Start/RouteConfig.cs
@@ -9,9 +9,24 @@ namespace Sample.Connector
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
             routes.Add(
-                "Default", 
-                new Route("{*path}", new DefaultRouteHandler())
+                "DefaultViews", 
+                new Route("views/{*path}", new DefaultRouteHandler())
+            );
+            
+            ///routes.Add(
+            ///    "Default", 
+            ///    new Route("{*path}", new DefaultRouteHandler())
+            ///);
+            
+            // should be last, after any custom route definition
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
             
         }
